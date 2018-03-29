b46763c4e5ea188cbe1a2c31c0584d32723e2195
BreD1810 <bradleybredgarrod@gmail.com>
2018-03-29 17:27:10 +0100

    Added 404 page

---
+++ not_found.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<?php
+include '/var/www/include.php';
+RenderHead('404 - Page not found');
+RenderNav('404');
+?>
+
+<body>
+
+<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    <img src="/images/404.png" style="height: 300px; width: auto; text-align: center;">
+    <main role="main" class="inner cover">
+    <h1 class="cover-heading">404 - Page Not Found!</h1>
+    <a class = "btn btn-lg btn-primary" href="/" role="button">Go to the homepage</a>
+</main>
+</div>
+
+</body>
+</html>
