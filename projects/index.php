80ecd1fc98e180e15af8eef5de2da550c4d32b55
Bradley Garrod <bradleybredgarrod@gmail.com>
2018-10-23 19:54:58 +0100

    Updated the projects page to be more relevant.

--- projects/index.php
+++ projects/index.php
@@ -14,7 +14,7 @@ RenderNav('projects');
 <div class="container">
     <h2>My Projects</h2>
     <div style="text-align: center;">
-        <p class="lead" style="text-align: center;">Currently I have a wide range of projects in a variety of languages. To view them, click the button below</p>
+        <p class="lead" style="text-align: center;">Currently I have a wide range of projects in a variety of languages. These are displayed on my Github page.</p>
         <a class = "btn btn-lg btn-primary" href="http://github.com/BreD1810" role="button" style="text-align: center;">Go to my GitHub</a>
     </div>
     <br>
@@ -25,12 +25,11 @@ RenderNav('projects');
     walking to the labs.", "https://github.com/BreD1810/CampusHack-2018", "CampusHack's Code");
     
     RenderProject("Connect 4", "In collaboration with Samuel Collins and Joel T. Parkinson, I have helped to produce a Connect 4 
-    game. Originally, this began as a simple C# console application. However, it is now being expanded to a Universa Windows Platform 
+    game. Originally, this began as a simple C# console application. However, it is now being expanded to a Universal Windows Platform 
     app. In the future, it will also become a mobile app built with Xamarin.", "https://github.com/GUIs-Are-For-the-Weak/Connect4", "Connect 4's Code");
 
-    RenderProject("Discord Bots", "I have created a series of Discord bots. In general, these are written in Python, and provide additional information 
-    when a user enters a link to the server, similar to the preview provided for certain sites.", "https://github.com/BreD1810/Discord-Bots", "Bots");
-
+    RenderProject("Haskell Programming", "As part of the Programming 3 module in the second year of my university course, I have completed several tasks 
+    using the Haskell programming language, which uses a functional paradigm.", "https://github.com/BreD1810/Programming-3-Labs", "Haskell Code");
     ?>
 </div>
 
