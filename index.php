e62ce6b4b1e8d91b9c03602b241474523fdda629
Bradley Garrod <bradleybredgarrod@gmail.com>
2018-11-23 16:18:02 +0000

    Updated homepage information and some styling - mobile styling is still not working :(

--- index.php
+++ index.php
@@ -17,10 +17,9 @@ RenderNav('home')
 <main role="main" class="inner cover">
     <h1 class="cover-heading">Bradley Garrod</h1>
     <p class="lead" style="font-family: 'Roboto', serif; font-size: 20px;">
-    I am a second year Computer Science Student at the University of Southampton.
-    Currently I am working on a variety of projects in multiple languages such as Java, C# and Haskell.
+    I am a second year Computer Science Student at the University of Southampton, working on a variety of projects in multiple languages, such as Java, C# and Haskell.
     I am currently working for RepairLabs, completing repairs on a variety of electronic devices.
-    In my free time, I enjoy competitive E-Sports, most notably reaching the second highest division in Team Fortress 2's European league. I also enjoy fishing, target shooting and cycling.
+    In my free time, I enjoy competitive E-Sports, most notably reaching the second highest division in Team Fortress 2's European league. I also enjoy fishing, jogging and cycling.
     </p>
 </main>
 </div>
