abd449a9f871873c9e0ee58524cab838cee01908
Bradley Garrod <bradleybredgarrod@gmail.com>
2019-02-20 10:56:14 +0000

    Minor changes from some time ago

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ RenderNav('home')
     <p class="lead" style="font-family: 'Roboto', serif; font-size: 20px;">
     I am a second year Computer Science Student at the University of Southampton, working on a variety of projects in multiple languages, such as Java, C# and Haskell.
     I am currently working for RepairLabs, completing repairs on a variety of electronic devices.
-    In my free time, I enjoy competitive E-Sports, most notably reaching the second highest division in Team Fortress 2's European league. I also enjoy fishing, jogging and cycling.
+    In my free time, I enjoy competitive E-Sports, most notably winning the mid division without losing a single game in Team Fortress 2's European league. I also enjoy fishing, jogging and cycling.
     </p>
 </main>
 </div>
